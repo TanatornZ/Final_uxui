aa3c0c2e85b09606ec5f45f4eb6484f7196d9232
TanatornZ <bank250145.bb@gmail.com>
2021-03-29 19:34:38 +0700

    create project

---
+++ components/navbar.vue
@@ -0,0 +1,24 @@
+<template >
+  <div class="w-full h-20 mx-auto px-20 py-3 bg-red-600 flex flex-wrap justify-center items-center ">
+      <div class="w-1/3">
+        <a href="https://github.com/TanatornZ" target="_blank" class="ml-4 text-lg font-medium">Github</a>
+     </div>
+     <div class="w-1/3 flex justify-center items-center" @click="$router.push('/')">
+        <button class="text-3xl font-medium text-white">SNEAKER</button>
+     </div>
+     <div class="w-1/3 flex justify-end" @click="$router.push('/Profile')">
+        <button class="mr-4 text-lg font-medium ">Profile</button>
+
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <center class="mt-10 ">
+      <img class="h-52 w-52 rounded-full border-4 shadow-2xl" :src="users.image" alt="">
+      <div class="text-2xl mt-5">id : {{users.username}}</div>
+      <div class="text-2xl mt-3"> {{users.firstname}} {{users.lastname}} </div>
+      <div class="text-xl mt-3" >Age : {{users.age}}</div>
+      </center>
+  </div>
+</template>
+
+<script>
+import user from "@/static/user.json"
+export default {
+  data:() => {
+    return( {
+      users:user
+    })
+  }
+
+}
+</script>
+
+<style>
+
+</style>
